bd2374a3e186a34a9cd0f51dc53d4a737d244ea1
meisu <michellecheung@1x-193-157-253-31.uio.no>
2014-09-10 15:50:27 +0200

    ferdig med hibernate oppgaven

--- src/main/java/no/uio/inf5750/assignment1/controller/BaseController.java
+++ src/main/java/no/uio/inf5750/assignment1/controller/BaseController.java
@@ -1,24 +1,88 @@
 package no.uio.inf5750.assignment1.controller;
-
+ 
+import no.uio.inf5750.assignment1.dao.MessageDao;
+import no.uio.inf5750.assignment1.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
  
 @Controller
+@RequestMapping("/")
 public class BaseController {
-
+ 
+        @Autowired
+        private MessageDao messageDao;
+        
+        /*
+         * Intro page. No database interaction.
+         */
         @RequestMapping(value="/", method = RequestMethod.GET)
         public String welcome(ModelMap model) {
-
-                model.addAttribute("message", "Maven Web Project + Spring 3 MVC - welcome()");
+ 
+                model.addAttribute("message", "Welcome to Michelle's page");
                 
                 //Spring uses InternalResourceViewResolver and return back index.jsp
                 return "index";
-
         }
-
+ 
+        /*
+         * Saves a message in the database
+         */
+        @RequestMapping(value="/send", method = RequestMethod.GET)
+        public String send(@RequestParam(value="content") String content, ModelMap model) {
+ 
+                Message msg = new Message();
+                msg.setContent(content);
+                int id = messageDao.save(msg);
+                model.addAttribute("message", "You have " + id + " stored messages");
+                return "index";
+ 
+        }
+        
+        /*
+         * Prints the last message entered
+         */
+        @RequestMapping(value="/read", method = RequestMethod.GET)
+        public String read(ModelMap model) {
+ 
+                Message message = messageDao.getLast();
+                if (message != null) {
+                        model.addAttribute("message", "The last message was: "+message.getContent());
+                }
+                else {
+                        model.addAttribute("message", "No message found");                        
+                }
+ 
+                //Spring uses InternalResourceViewResolver and return back index.jsp
+                return "index";
+        }
+        
+        /*
+         * Prints the message with a given id
+         */
+        @RequestMapping(value="/read/{id}", method = RequestMethod.GET)
+        public String readId(@PathVariable int id, ModelMap model) {
+ 
+                Message message = messageDao.get(id);
+                if (message != null) {
+                        model.addAttribute("message", "Message number "+id+" was: "+message.getContent());
+                }
+                else {
+                        model.addAttribute("message", "No message found");                        
+                }
+ 
+                //Spring uses InternalResourceViewResolver and return back index.jsp
+                return "index";
+        }
+        
+        /*
+         * sidene welcome og hello 
+         */
+        
         @RequestMapping(value="/welcome/{name}", method = RequestMethod.GET)
         public String welcomeName(@PathVariable String name, ModelMap model) {
 
@@ -34,6 +98,7 @@ public class BaseController {
                 model.addAttribute("navn", "" + name);
 
                 return "hello";
-        }
-
+        }     
+        
 }
+
--- src/main/webapp/WEB-INF/pages/index.jsp
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -1,8 +1,26 @@
+
 <html>
-<body>
-<h2>Hello World!</h2>
+<style>
+body {
+ 	background-color:#bfea30; 
+	text-align:center;
+	font-family:tahoma;
+}
 
-<h4>Message : ${message}</h4>   
+h1 {
+	color:red;
+	}
 
+</style>
+
+<body> 
+<h1>${message}</h1>
+<h2>Leave a message</h2>
+ 
+<form name="input" action="/assignment1_michelch/send" method="get">
+Message content: <input type="text" name="content">
+<input type="submit" value="Submit">
+</form>
+<p><a href="/assignment1_michelch/read">Get the latest message</a></p>
 </body>
 </html>
